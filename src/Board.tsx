5741f663ea88b9fbb1c69932b893495944253a83
Javier Pérez Arteaga <surtich@gmail.com>
2024-10-18 12:44:15 +0200

    Se usa map para permitir tableros dinámicos.

--- src/Board.tsx
+++ src/Board.tsx
@@ -4,9 +4,14 @@ import BoardRow from "./BoardRow";
 
 export type Value = "X" | "O" | null;
 
+const rows = 3;
+const cols = 3;
+
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState<Value[]>(Array(9).fill(null));
+  const [squares, setSquares] = useState<Value[]>(
+    Array(rows * cols).fill(null)
+  );
 
   const winner = calculateWinner(squares);
 
@@ -24,26 +29,29 @@ export default function Board() {
       <div className="status">
         {winner ? "Winner: " + winner : "Next player: " + (xIsNext ? "X" : "O")}
       </div>
-      <BoardRow>
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </BoardRow>
-      <BoardRow>
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </BoardRow>
-      <BoardRow>
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </BoardRow>
+      {Array(rows)
+        .fill(null)
+        .map((_v, r) => (
+          <BoardRow key={r}>
+            {Array(cols)
+              .fill(null)
+              .map((_v, c) => {
+                const key = c + cols * r;
+                return (
+                  <Square
+                    key={key}
+                    value={squares[key]}
+                    onSquareClick={() => handleClick(key)}
+                  />
+                );
+              })}
+          </BoardRow>
+        ))}
     </>
   );
 }
 
-function calculateWinner(squares) {
+function calculateWinner(squares: Value[]) {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
