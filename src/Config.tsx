cbeb4c07ba0835b68a8210fa4e572f4e766f5d68
Javier Pérez Arteaga <surtich@gmail.com>
2024-10-21 19:01:25 +0200

    Se evita que se pueda añadir un tablero si la configuración es inválida.

--- src/Config.tsx
+++ src/Config.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import type { Config } from "./App";
 
 type ConfigProps = {
@@ -6,6 +6,7 @@ type ConfigProps = {
 };
 
 export default function Config({ addGame }: ConfigProps) {
+  const [disabledButton, setDisabledButton] = useState(false);
   const rowsRef = useRef<HTMLInputElement>(null);
   const colsRef = useRef<HTMLInputElement>(null);
 
@@ -16,17 +17,43 @@ export default function Config({ addGame }: ConfigProps) {
     addGame(rows, cols);
   }
 
+  function isValidConfig() {
+    const rows = parseInt(rowsRef.current!.value, 10);
+    const cols = parseInt(colsRef.current!.value, 10);
+    return !isNaN(rows) && !isNaN(cols);
+  }
+
+  function handleChange() {
+    setDisabledButton(!isValidConfig());
+  }
+
   return (
     <form>
       <label>
         Rows:
-        <input type="number" min={1} max={10} defaultValue={3} ref={rowsRef} />
+        <input
+          type="number"
+          min={1}
+          max={10}
+          defaultValue={3}
+          ref={rowsRef}
+          onChange={handleChange}
+        />
       </label>
       <label>
         Cols:
-        <input type="number" min={1} max={10} defaultValue={3} ref={colsRef} />
+        <input
+          type="number"
+          min={1}
+          max={10}
+          defaultValue={3}
+          ref={colsRef}
+          onChange={handleChange}
+        />
       </label>
-      <button onClick={handleClick}>Add game</button>
+      <button onClick={handleClick} disabled={disabledButton}>
+        Add game
+      </button>
     </form>
   );
 }
