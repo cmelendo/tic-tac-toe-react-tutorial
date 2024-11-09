d1f4a12ca38efa6031c034bb2faae6d06eb10b66
Carlos Melendo Bojo <carlos.melendo.bojo@gmail.com>
2024-11-09 17:54:47 +0100

    Añadidos enunciados de los ejercicios

--- src/App.tsx
+++ src/App.tsx
@@ -46,3 +46,18 @@ function App() {
 }
 
 export default App;
+
+//Ejercicio: Eliminar un juego o nuevo tablero
+
+//Ejercicio: Junto a menuitem tendra un botón que permitirá borrar ese menu item (NO ELIMINAR CONFIG, No pasarle el boton de borrar, prop menuitem de game no config)
+
+//Ejercicio: Misma funcionalidad que el anterior pero el botón de borrar en game (De hijo a padre), game no deebería saber nada o poco de menuItem, por si cambias la
+//estructura del padre de game
+
+//Ejercicio: Cuando clickas game se abre, pero tiene que hacer que al click si está abierto se cierre (Mirar propiedad active para poner null)
+
+//Ejercicio: Si quiero tener varios tableros abiertos a la vez (Con array de actives, en vez de un string solo)
+
+//Ejercicio: Poner clase en el game abierto o seleccionado para que tenga un color resaltado
+
+//Ejercicio: Cuando haya jugada ganadora que te cambie el fondo de la combinación ganadora (Cuidado con el historial)
